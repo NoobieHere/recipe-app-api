169889a01045fdf484ab7d3aebc9b75c72163736
Jep <mynameisjep14@gmail.com>
2022-05-28 12:31:11 +0800

    Added wait_for_db command

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Functions
+"""
+
+
+def add(a, b):
+    """ Add a and b return result. """
+    return a + b
+
+
+def subtract(a, b):
+    """ Subtract a from b return result. """
+    return b - a
